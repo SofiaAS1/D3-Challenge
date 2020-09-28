4265242f978c7b644c85ee6a319dd202211c7d10
SAS 826 <sasotillo826@gmail.com>
2020-09-28 13:35:23 -0400

    HW so far

--- D3_Data_Journalism/StarterCode/assets/js/app.js
+++ D3_Data_Journalism/StarterCode/assets/js/app.js
@@ -184,13 +184,14 @@ d3.csv("assets/data/data.csv").then(function(fullData, err) {
     .attr("opacity", ".5");
 
 
-  var states = circlesGroup.append("text")
+  var states = circlesGroup.selectAll("text")
     .data(fullData)
     .enter()
+    .append("text")
     // .attr("x", d => xLinearScale(d[chosenXAxis]))
     // .attr("y", d => yLinearScale(d.healthcare))
     .text(d => d.abbr)
-    .attr('fill-opacity', 5)
+    // .attr('fill-opacity', 5)
     .attr('fill', 'teal')
   
   
